503c20743e70429ed947f610689f9f472dba8432
GroupWeb21 <80159815+ThanhNghia-JV4@users.noreply.github.com>
2023-04-22 18:53:10 +0700

    Update Home.jsx

--- client/thirdweb-app/src/pages/Home.jsx
+++ client/thirdweb-app/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { DisplayCampaigns } from '../components';
 import { useStateContext } from '../context'
 
-const Home = () => {z
+const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [campaigns, setCampaigns] = useState([]);
 
